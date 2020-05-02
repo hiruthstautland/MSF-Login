1dbd9c91836c8db09e312aefba0df0bad2cb2155
Hiruth Marie Stautland <hiruthstautland@gmail.com>
2020-05-02 12:43:01 +0200

    added userList component for displaying

---
+++ src/components/ActiveUsers/UserList.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+export const UserList = ({ users }) => {
+  return (
+    <table className="user-table">
+      <thead className="user-table__categories">
+        <tr>
+          <th />
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => {
+          return (
+            <tr key={user.id}>
+              <td>Innlogget?</td>
+              <td className="user-table__user">
+                <Link to={`/user/${user.slug}`}>{user.name}</Link>
+              </td>
+              <td>{user.email}</td>
+              <td>{user.userRole}</td>
+              <td>{user.userRole}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+UserList.propTypes = {
+  users: PropTypes.array.isRequired,
+};
